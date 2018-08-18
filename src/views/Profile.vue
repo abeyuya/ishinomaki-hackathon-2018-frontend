9fac045a77c5ddc5e6dec66aa64dc61de77cef3b
Toshichika Yamashita <t-yamashita@grune.co.jp>
2018-08-19 02:44:47 +0900

    登録フォームをざっくり修正

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -6,25 +6,31 @@
         <img src="https://yt3.ggpht.com/a-/ACSszfHx5NjkvUKI-erg6MSo3-MeZgEWOZgFTK8orQ=s900-mo-c-c0xffffffff-rj-k-no">
         <div class="profile">
             <p>お名前</p>
-            <span><input v-model="name"></span>
+            <input type="text" v-model="name" placeholder="石巻太郎">
             <p>あだ名</p>
-            <span><input v-model="nickname"></span>
-            <p>やりたい役職</p>
-            <span><input v-model="role"></span>
+            <input type="text" v-model="nickname" placeholder="ハック">
+            <p>やりたい役割</p>
+            <input type="text" v-model="role" placeholder="プログラマ">
             <p>スキル</p>
-            <span><input v-model="skill"></span>
+            <input type="text" v-model="skill" placeholder="Java, Kotlin, Python, Processing, Swift">
             <p>所属</p>
-            <span><input v-model="organization"></span>
+            <input type="text" v-model="organization" placeholder="GDG石巻">
             <p>参加したきっかけ</p>
-            <span><input v-model="purpose"></span>
+            <textarea v-model="purpose" rows="2" placeholder="いろいろなエンジニアの方と開発を行って刺激をうけにきました。"/>
             <p>その他</p>
-            <span><input v-model="none"></span>
-            <p>アイディアの有無</p>
+            <textarea v-model="none" rows="2" placeholder="石巻ハッカソンに毎年参加してます。Android大好きです"/>
+            <p>アイデア名</p>
+            <input type="text" v-model="todomodelname" placeholder="Hackage(ハッケージ)">
+            <p>アイデア概要</p>
+            <textarea v-model="todomodelname" rows="2" placeholder="ハッピー+ハッカソン+パッケージでハッカソンをハック"/>
+            <p>想定している技術</p>
+            <input type="text" v-model="todomodelname" placeholder="Vue.js, Firebase">
+            <!-- <p>アイディアの有無</p>
             <label><input type="radio" v-model="radio" value="Yes">ある</label>
             <label><input type="radio" v-model="radio" value="No">ない</label>
-            <p>{{radio}}</p>
+            <p>{{radio}}</p> -->
         </div>
-        <md-button class="md-raised" @click="join">続ける</md-button>
+        <md-button class="md-raised save" @click="join">保存</md-button>
     </div>
 </template>
 
@@ -51,24 +57,43 @@ export default class Profile extends Vue {
 <style lang="scss" scoped>
   header{
     height: 4rem;
-    background-color: #33CC66;
+    background-color: #52B574;
   }
   h1{
-      color:#33CC66;
+      color:#52B574;
   }
   span{
-      border-bottom: solid 0.3rem #33CC66;
+      border-bottom: solid 0.3rem #52B574;
   }
   img{
     height: 2rem;
   }
   p{
-      color:#33CC66;
+      color:#52B574;
       padding-left: 0;
+          margin-bottom: 30px;
   }
   .md-button{
-      background-color:#33CC66;
+      background-color:#52B574;
       color: white;
       border-radius: 1rem;
   }
+  input[type="text"], textarea{
+    border: 0;
+    outline: 0;
+    background: transparent;
+    border-bottom: 2px solid #52B574;
+    width: 60%;
+    font-size: 20px;
+    text-align: center;
+  }
+
+  textarea{
+    font-size: 16px;
+  }
+
+  .save{
+      margin-top: 40px;
+  }
+  
 </style>
