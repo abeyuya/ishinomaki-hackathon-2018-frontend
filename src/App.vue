6d64820a3a9bd18faf6742a7e9953c34e7282aa1
abeyuya <yuya.abe.0525@gmail.com>
2018-08-18 17:54:18 +0900

    material他の箇所でも使えるように

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,16 @@
   </div>
 </template>
 
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+
+Vue.use(VueMaterial)
+
+export default class App extends Vue {}
+</script>
+
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -18,11 +18,8 @@ import { Component, Vue } from 'vue-property-decorator'
 import { firebase } from '../lib/firebase'
 import User from '../model/user'
 import ProjectCard from '@/components/ProjectCard.vue'
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
 import Project from '../model/project'
 
-Vue.use(VueMaterial)
 const provider = new firebase.auth.GithubAuthProvider()
 
 @Component({
