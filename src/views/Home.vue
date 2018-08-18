92b50d2400bf1be2d012fb68b17e1bfc7f17580c
abeyuya <yuya.abe.0525@gmail.com>
2018-08-18 11:59:20 +0900

    home調整

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,15 +12,40 @@
     <router-link tag="li" to="/projects">
       <a>/projects</a>
     </router-link>
+    <hr>
+    <div>
+      <h3>ログイン状態</h3>
+      <div v-if="user">
+        ログイン中です
+        {{ this.user }}
+      </div>
+      <div v-else>
+        Loading...
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import { firebase } from '../lib/firebase'
+import User from '../model/user'
 
 @Component({
   components: {
   },
   })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  user: User | null = null
+
+  created () {
+    firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        this.user = await User.findByUid(user.uid)
+      } else {
+        this.user = null
+      }
+    })
+  }
+}
 </script>
