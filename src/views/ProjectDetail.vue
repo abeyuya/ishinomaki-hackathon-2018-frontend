49c578d6684482aac167d845d5b18f56be3b23a7
Yasuhiro Yaguchi <paiguchi@gmail.com>
2018-08-18 22:45:49 +0900

    cssの追加

--- src/views/ProjectDetail.vue
+++ src/views/ProjectDetail.vue
@@ -3,15 +3,17 @@
   <div v-if="user">
     <div class="project">
         <div class="project_detail">
-            <h3>{{project_title}}</h3>
-            <h4>{{owner_name}}</h4>
-            <button class="join" @click="join">join</button>
-            <h4>概要</h4>
+            <h1 class="title">{{project_title}}</h1>
+            <h2 class="owner_name">{{owner_name}}</h2>
+            <md-button class="join" @click="join">Join</md-button>
+            <h2>概要</h2>
             <p>{{overview}}</p>
-            <h4>想定している技術</h4>
-            <p>{{need_skills}}</p>
-            <h4>メンバー</h4>
-            <button class="join" @click="join">join</button>
+            <h2>想定している技術</h2>
+            <div class="flexbox">
+              <p class="need_skills">{{need_skills}}</p>
+            </div>
+            <h2>メンバー</h2>
+            <md-button class="join" @click="join">Join</md-button>
         </div>
     </div>
   </div>
@@ -73,5 +75,57 @@ export default class ProjectDetail extends Vue {
     console.log('保存しました todo: トーストみたいなので表示したい')
   }
 }
-
 </script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+  h1.title {
+    color: #52B574;
+    margin: 40px 0 0;
+    font-weight: 900;
+    font-size: 30px;
+  }
+
+  h2 {
+    color: #52B574;
+    font-weight: 300;
+    margin: 50px 0 0;
+    font-size: 20px;
+  }
+
+  h2.owner_name {
+    font-weight: 200;
+    margin: 10px 0 0;
+  }
+
+  p {
+    color: #B4B4B4;
+  }
+
+  p.need_skills {
+    font-weight: 300;
+    width: 70vw;
+    border: 1px solid #B1EAD3;
+    border-style: none none solid none;
+    padding: 0px 0px 10px 0px;
+  }
+
+  .flexbox {
+    display: -webkit-flex;
+    display: flex;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-align-items: stretch;
+    align-items: stretch;
+    text-align: center;
+  }
+
+  .md-button {
+    color: white;
+    background-color: #F3933D;
+    width: 40vw;
+    height: 48px;
+    margin: 40px 0px 0px 0px;
+  }
+</style>
