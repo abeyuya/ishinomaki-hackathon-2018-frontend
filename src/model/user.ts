0cbd89a9e8bdabef5882f6e466ae47b39482e0ee
abeyuya <yuya.abe.0525@gmail.com>
2018-08-18 11:01:46 +0900

    logoutできるように

--- src/model/user.ts
+++ src/model/user.ts
@@ -4,16 +4,24 @@ import { db } from '../lib/firebase'
 export default class User {
   /* eslint-disable */
   uid?: string;
-  icon_url?: string;
+  photo_url?: string;
+  user_name?: string;
+  nickname?: string;
+  role?: string;
+  skill?: string;
+  organization?: string;
+  purpose?: string;
+  note?: string;
   /* eslint-enable */
 
   constructor (json: object) {
     Object.assign(this, json)
   }
 
-  public static async create (param: { uid: string, icon_url?: string }): Promise<User> {
-    await db.collection('users').doc(param.uid).set(param, { merge: true })
+  public static async create (param: { uid: string, photo_url?: string }): Promise<User> {
     const user = new User(param)
+    const obj = Object.assign({}, user)
+    await db.collection('users').doc(param.uid).set(obj, { merge: true })
     return user
   }
 }
---
+++ src/views/Logout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { firebase } from '../lib/firebase'
+import User from '../model/user'
+
+const provider = new firebase.auth.GithubAuthProvider()
+
+@Component
+export default class Login extends Vue {
+  async created () {
+    await firebase.auth().signOut()
+    this.$router.push('/login')
+  }
+}
+</script>
